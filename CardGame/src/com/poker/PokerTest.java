7fb72bab6a16a75982d86425c9136dbdd0f6f9f9
anirudhit <anirudhit@outlook.com>
2017-11-18 22:54:59 +0200

    Implementing the game categories

--- CardGame/src/com/poker/PokerTest.java
+++ CardGame/src/com/poker/PokerTest.java
@@ -1,8 +1,11 @@
 package com.poker;
 public class PokerTest {
 	private static Deck cards;
+	private static Game gm;
     public static void main(String[] args){
     	cards = new Deck();
     	cards.buildDeck();
+    	gm = new Game();
+    	gm.startGame();
     }
 }
